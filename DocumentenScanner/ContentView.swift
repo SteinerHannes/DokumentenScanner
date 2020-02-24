a582df27bca432cbc16e14b3987ab5706b82469f
Hannes Steiner <Hannes.Steiner@web.de>
2020-02-24 11:59:01 +0100

    Initial Commit

---
+++ DocumentenScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DocumentenScanner
+//
+//  Created by Hannes Steiner on 24.02.20.
+//  Copyright © 2020 Hannes Steiner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
