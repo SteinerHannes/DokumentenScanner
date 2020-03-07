d2c63826b56b6d34c137c47869999ff30ac1e48b
Hannes Steiner <Hannes.Steiner@web.de>
2020-03-07 13:30:21 +0100

    Added comments on Templates and minor changes

--- DocumentenScanner/Templates/CreateTemplateView.swift
+++ DocumentenScanner/Templates/CreateTemplateView.swift
@@ -17,21 +17,22 @@ struct CreateTemplateView: View {
 
     @State var isBottomSheetOpen: Bool = true
 
-    //    init() {
-    //        UITableView.appearance().backgroundColor = .clear // tableview background
-    //        //UITableViewCell.appearance().backgroundColor = .clear // cell background
-    //    }
-
     private var scale: CGFloat {
+        // if the images height is greater then the image width * 16/9 screen size
         if self.appState.currentTemplate!.pages[self.index].image.size.height >
             self.appState.currentTemplate!.pages[self.index].image.size.width * (16/9) {
             var tempHeight: CGFloat = 0.0
             if #available(iOS 13.0, *) {
                 tempHeight += UIApplication.shared.windows.first?.safeAreaInsets.bottom ?? 0.0
             }
+            // shrink the image to fit inside the safe area with some padding
+            // 10 := beacuse of the bottom sheet indicator
+            // 80 := bottom and top padding
+            // (screen height - some padding) / image height
             return (UIScreen.main.bounds.height - ((2 * tempHeight) + 10 + 80)) /
                 self.appState.currentTemplate!.pages[self.index].image.size.height
         }
+        // screen width / image width
         return UIScreen.main.bounds.width / self.appState.currentTemplate!.pages[self.index].image.size.width
     }
 
@@ -72,20 +73,19 @@ struct CreateTemplateView: View {
                     Section {
                         //swiftlint:disable line_length
                         ForEach(self.appState.currentTemplate!.pages[self.index].regions, id: \.id) { region in
+                        //swiftlint:enable line_length
                             Text(region.name)
                                 .contextMenu {
                                     Button(action: {
-                                        // self.page.deletRegions(for: id)
                                         self.deleteAttribute(for: region.id)
                                     }) {
-                                        // MARK: no effect
+                                        // MARK: no size and color effect
                                         Text("Löschen").font(.system(size: 15))
                                         Image(systemName: "trash").font(.system(size: 15))
                                             .foregroundColor(.red)
                                     }
                             }
                         }
-                        //swiftlint:enable line_length
                     }
                 }
                 .listStyle(GroupedListStyle())
@@ -96,21 +96,31 @@ struct CreateTemplateView: View {
         .navigationBarTitle("Attribute hinzufügen", displayMode: .inline)
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
         .onAppear {
+            // set the image to the current page of the template
             self.appState.image = self.appState.currentTemplate!.pages[self.index].image
+            // set the index in the appstate for the child views
             self.appState.currentPage = self.index
+            // set the max height to the min(3 , regions in the image)
             self.appState.maxHeight = 140.0 + CGFloat(45 *
                 min(self.appState.currentTemplate!.pages[self.appState.currentPage!].regions.count, 3))
         }
     }
 
+    /**
+     Delete the attribute in the list of attribute of the current page in the app state
+     - parameter id: The unique id of the attribute
+     */
     func deleteAttribute(for id: String) {
+        // uses the current page number to delete the attribute in the app state
         if let index = self.appState.currentTemplate!.pages[self.index].regions.firstIndex(where: {
             $0.id == id
         }) {
             self.appState.currentTemplate!.pages[self.index].regions.remove(at: index)
-        }
-        if self.appState.maxHeight > 140 {
-            self.appState.maxHeight -= 45
+            // shrink the height of the bottom sheet, if there are less than 4 itmes in it
+            if self.appState.maxHeight > 140 &&
+                self.appState.currentTemplate!.pages[self.index].regions.count < 3 {
+                self.appState.maxHeight -= 45
+            }
         }
     }
 }
