17daf42850090fb920d85e7cac87465bb005bffc
Hannes Steiner <Hannes.Steiner@web.de>
2020-03-18 12:07:49 +0100

    Updated AppStoreMock

--- DocumentenScanner/Mock/AppStoreMock.swift
+++ DocumentenScanner/Mock/AppStoreMock.swift
@@ -20,7 +20,7 @@ class AppStoreMock {
         // set current template
         appState.currentTemplate = template
         // add templte to templates
-        appState.teamplates.append(template)
+        //appState.teamplates.append(template)
         // set a new template to the test template
         appState.newTemplateState.newTemplate = template
         // set a test image
@@ -30,18 +30,31 @@ class AppStoreMock {
         return store
     }
 
+    private static func regions1() -> [ImageRegion] {
+        let region1 = ImageRegion(name: "Note", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .mark)
+        let region2 = ImageRegion(name: "Seminargruppe", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .seminarGroup)
+        let region3 = ImageRegion(name: "Matrikelnummer", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .studentNumber)
+        return [region1, region2, region3]
+    }
+
+    private static func regions2() -> [ImageRegion] {
+        let region1 = ImageRegion(name: "Test", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .mark)
+        let region2 = ImageRegion(name: "Bla", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .seminarGroup)
+        let region3 = ImageRegion(name: "Naja", rectState: CGSize(width: 10, height: 10),
+                                  width: 50, height: 100, datatype: .studentNumber)
+        return [region1, region2, region3]
+    }
+
     private static func pages() -> [Page] {
-        let page1 = Page(id: 0, image: UIImage(imageLiteralResourceName: "test"))
-        let page2 = Page(id: 1, image: UIImage(imageLiteralResourceName: "post"))
-        let page3 = Page(id: 2, image: UIImage(imageLiteralResourceName: "klausur1"))
-        let page4 = Page(id: 3, image: UIImage(imageLiteralResourceName: "klausur2"))
-
-        var list: [Page] = []
-        list.append(page1)
-        list.append(page2)
-        list.append(page3)
-        list.append(page4)
-
-        return list
+        let page1 = Page(id: 0, image: UIImage(imageLiteralResourceName: "test"), regions: regions1())
+        let page2 = Page(id: 1, image: UIImage(imageLiteralResourceName: "post"), regions: regions2())
+        let page3 = Page(id: 2, image: UIImage(imageLiteralResourceName: "klausur1"), regions: regions1())
+        let page4 = Page(id: 3, image: UIImage(imageLiteralResourceName: "klausur2"), regions: regions2())
+        return [page1, page2, page3, page4]
     }
 }
