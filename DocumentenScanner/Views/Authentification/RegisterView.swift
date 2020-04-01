eb02ae98ec35fa7b80f72cd8ff1236934f47f749
Hannes Steiner <Hannes.Steiner@web.de>
2020-04-01 17:14:59 +0200

    Added RegisterView

---
+++ DocumentenScanner/Views/Authentification/RegisterView.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterView.swift
+//  DocumentenScanner
+//
+//  Created by Hannes Steiner on 01.04.20.
+//  Copyright © 2020 Hannes Steiner. All rights reserved.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
